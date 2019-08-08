0c9b2ad2d51e8f9adabc9de19c10e4f795bca9b8
Anirudhsfc <anirudhsfc@gmail.com>
2019-08-08 12:15:07 +0530

    Backend implementation

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -3,9 +3,11 @@ import { version, Button, Input, PageHeader, Timeline } from 'antd'
 import "antd/dist/antd.css";
 import { Redirect } from 'react-router-dom'
 import Card from '../components/Card'
-import config from '../config';
+// import config from '../config';
+import Tabletop from 'tabletop'
 // import load from '../spreadsheet';
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId}/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${config.apiKey}`;
+// const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId}/values:batchGet?ranges=Sheet4&majorDimension=ROWS&key=${config.apiKey}`;
+const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1hpvyH-a7TLNuAsHThBGew5xbUFF5FcJzCZfLCXX0fek/edit?usp=sharing';
 
 class AccountPage extends Component {
 
@@ -16,38 +18,56 @@ class AccountPage extends Component {
         };
     }
     componentDidMount() {
-        console.log("inside component did mount")
-        fetch(url).then(response => response.json()).then(data => {
-            let batchRowValues = data.valueRanges[0].values;
-            // console.log("batchRowValues=" + batchRowValues);
-            const rows = [];
-            // console.log("rows arr=" + rows);
-            for (let i = 1; i < batchRowValues.length; i++) {
-                let rowObject = {};
-                // console.log("rowObj=" + rowObject);
-                for (let j = 0; j < batchRowValues[i].length; j++) {
-                    rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
-                }
-                rows.push(rowObject);
-            }
-            for (let i = 0; i < rows.length; i++) {
-                // console.log(rows[i])
-            }
-            this.setState({ items: rows });
+        this.init();
+    }
+    // // console.log("inside component did mount")
+    // fetch(url).then(response => response.json()).then(data => {
+    //     let batchRowValues = data.valueRanges[0].values;
+    //     // console.log("batchRowValues=" + batchRowValues);
+    //     const rows = [];
+    //     // console.log("rows arr=" + rows);
+    //     for (let i = 1; i < batchRowValues.length; i++) {
+    //         let rowObject = {};
+    //         // console.log("rowObj=" + rowObject);
+    //         for (let j = 0; j < batchRowValues[i].length; j++) {
+    //             rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+    //         }
+    //         rows.push(rowObject);
+    //     }
+    //     for (let i = 0; i < rows.length; i++) {
+    //         // console.log(rows[i])
+    //     }
+    //     this.setState({ items: rows });
 
+    // });
+    init() {
+        console.log("inside init")
+        Tabletop.init({
+            key: publicSpreadsheetUrl,
+            callback: this.showInfo,
+            simpleSheet: true
+        })
+    }
+    showInfo(data, tabletop) {
+        console.log("successful")
+        // alert('Successfully processed!')
+        console.log(data);
+        data.forEach(element => {
+            // console.log("okay")
+            console.log(element.CourseCode);
         });
     }
 
 
     render() {
-        return(
+        return (
             <div>
-           {this.state.items.map(item =>
-            
-            <Card title={item.Name}></Card>
-             
-           )} 
-       </div>
+                {this.state.items.map(item =>
+
+                    <Card title={item.Name}></Card>
+
+                )}
+            </div>
         )
     }
 

378dc872be9774d58a8aa8ae4b0b6f6ba2f233ae
Anirudhsfc <anirudhsfc@gmail.com>
2019-08-09 01:34:39 +0530

    Publishing

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 import './App.css';
 import MainPage from '../src/pages/MainPage'
-import {Route,Switch} from 'react-router-dom'
+import { Route, Switch, Link, BrowserRouter } from 'react-router-dom'
 import Page1 from './pages/Page1.js'
 import AccountPage from './pages/AccountPage'
 import LoginPage from './pages/LoginPage'
+
+console.log("inside App.js")
 function App() {
   return (
-    
-    <Switch>
-    <Route path="/" exact component={MainPage}/>
-     <Route path="/mainPage"  component={MainPage}/>
-    <Route path="/coursePage1" component={Page1}/>
-    <Route path="/account" component={AccountPage}/>
-    </Switch>
-     
-   
+
+    <BrowserRouter basename="/ChooseWisely">
+      {/* <Link to="/">Home</Link>
+      <Link to="/mainPage">MainPage</Link> */}
+      <Route path="/" exact component={MainPage} />
+      <Route path="/mainPage" component={MainPage} />
+      <Route path="/coursePage1" component={Page1} />
+      <Route path="/account" component={AccountPage} />
+
+    </BrowserRouter>
+
   );
 }
 

d6c1bc8e825421a1a26656f7e26a489255911fa0
Anirudhsfc <anirudhsfc@gmail.com>
2019-08-09 11:38:04 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Button from 'antd'
 import './App.css';
 import MainPage from '../src/pages/MainPage'
 import { Route, Switch, Link, BrowserRouter } from 'react-router-dom'
@@ -11,15 +11,16 @@ console.log("inside App.js")
 function App() {
   return (
 
-    <BrowserRouter basename="/ChooseWisely">
-      <Link to="/">Home</Link>
-      <Link to="/mainPage">MainPage</Link>
-      <Route path="/" exact component={MainPage} />
-      <Route path="/mainPage" component={MainPage} />
-      <Route path="/coursePage1" component={Page1} />
-      <Route path="/account" component={AccountPage} />
+    // <BrowserRouter basename="/ChooseWisely">
+    //   <Link to="/">Home</Link>
+    //   <Link to="/mainPage">MainPage</Link>
+    //   <Route path="/" exact component={MainPage} />
+    //   <Route path="/mainPage" component={MainPage} />
+    //   <Route path="/coursePage1" component={Page1} />
+    //   <Route path="/account" component={AccountPage} />
 
-    </BrowserRouter>
+    // </BrowserRouter>
+    <Button type="primary" size="large">HI I AM PUBLISHED</Button>
 
   );
 }

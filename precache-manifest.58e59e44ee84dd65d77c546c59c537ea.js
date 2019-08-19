3314aa7ca6aa8d3adb9ba6eccf095f2ecbca2796
Anirudhsfc <anirudhsfc@gmail.com>
2019-08-19 21:30:54 +0530

    Updates

---
+++ precache-manifest.58e59e44ee84dd65d77c546c59c537ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744ff6451468cd9bc4fe74eded3d57d4",
+    "url": "/ChooseWisely/index.html"
+  },
+  {
+    "revision": "c83985339f1abf4d9a7b",
+    "url": "/ChooseWisely/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "f9dc25e39fafec760505",
+    "url": "/ChooseWisely/static/css/main.cf41bddf.chunk.css"
+  },
+  {
+    "revision": "c83985339f1abf4d9a7b",
+    "url": "/ChooseWisely/static/js/2.21d4f19c.chunk.js"
+  },
+  {
+    "revision": "f9dc25e39fafec760505",
+    "url": "/ChooseWisely/static/js/main.707c2a52.chunk.js"
+  },
+  {
+    "revision": "ed73b2011ff3fda2301f",
+    "url": "/ChooseWisely/static/js/runtime~main.de24689b.js"
+  },
+  {
+    "revision": "c03411a1e73d4f7122302cd2e5d730ee",
+    "url": "/ChooseWisely/static/media/messi.c03411a1.jpg"
+  }
+]);
